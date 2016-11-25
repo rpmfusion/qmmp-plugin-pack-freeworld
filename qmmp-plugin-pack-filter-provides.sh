36729f4d2f7313815c2b0a5b5ba3862b832b200a
Karel Volny <kvolny@redhat.com>
2016-11-25 13:07:18 +0100

    retire, mpg123 provided in Fedora

--- qmmp-plugin-pack-filter-provides.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -x /usr/lib/rpm/redhat/find-provides ]; then
-    FINDPROV=/usr/lib/rpm/redhat/find-provides
-else
-    FINDPROV=/usr/lib/rpm/find-provides
-fi
-      
-$FINDPROV $* | sed -e '/libmpg123.so/d'
